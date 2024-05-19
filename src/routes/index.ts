7c6a9f694131d0fefda027d728af0fe527e5f88e
Chaika Andrey <hahol86@narod.ru>
2024-05-19 12:36:53 +0300

    feat: connect `orgs` route to tree routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,16 @@ import cookieParser from 'cookie-parser';
 import { Auth } from '@app/middlewares';
 import { appController } from '@app/controllers';
 
+import roueteOrgs from './api/orgs';
+
 const router = Router();
 const auth = new Auth();
 
 router.get('/healthz', appController.healthz);
 router.use('/auth', cookieParser(), authRoutes);
 router.use('/api', auth.authenticate, api);
+
+/* temp connect 'orgs' for debug*/
+router.use('/orgs', roueteOrgs);
 
 export default router;
