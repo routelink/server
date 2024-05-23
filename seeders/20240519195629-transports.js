99c7d78f59425e4818b1a40e3979bc0b696b9422
olegnowikow <nowolegik@gmail.com>
2024-05-23 22:55:25 +0300

    fix: seeders transports

--- seeders/20240519195629-transports.js
+++ seeders/20240519195629-transports.js
@@ -86,14 +86,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  },
-};
     await queryInterface.bulkDelete('transports', null, {});
   },
 };
