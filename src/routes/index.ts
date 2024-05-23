239695ddb32fbf5e09d7bbda56e919c360514603
ekaterina-pa <2ksu.tei@gmail.com>
2024-05-23 12:18:49 +0300

    add migration, controller, model, route, service

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import api from './api';
 import cookieParser from 'cookie-parser';
 import { Auth } from '@app/middlewares';
 import { appController } from '@app/controllers';
+import routeEmployees from './api/employees';
 
 const router = Router();
 const auth = new Auth();
@@ -11,5 +12,6 @@ const auth = new Auth();
 router.get('/healthz', appController.healthz);
 router.use('/auth', cookieParser(), authRoutes);
 router.use('/api', auth.authenticate, api);
+router.use('/employees', routeEmployees);
 
 export default router;
