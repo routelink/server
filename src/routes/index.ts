28f03dda9ee67876f31956d9c5ca1e3ab096e1a6
TheKenkei <nowikow.witaliy@gmail.com>
2024-05-25 14:48:49 +0300

    chore: uncomment auth middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,19 @@
-import { Router } from 'express';
-import authRoutes from './auth';
-import api from './api';
 import cookieParser from 'cookie-parser';
-//import { Auth } from '@app/middlewares';
+import { Router } from 'express';
+
 import { appController } from '@app/controllers';
+import { Auth } from '@app/middlewares';
+
+import api from './api';
 import routeEmployees from './api/employees';
+import authRoutes from './auth';
 
 const router = Router();
-//const auth = new Auth();
+const auth = new Auth();
 
 router.get('/healthz', appController.healthz);
 router.use('/auth', cookieParser(), authRoutes);
-router.use('/api', /* auth.authenticate,*/ api);
+router.use('/api', auth.authenticate, api);
 router.use('/employees', routeEmployees);
 
 export default router;
