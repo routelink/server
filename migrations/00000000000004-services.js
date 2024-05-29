2b39826aff41a9e404a17b7131fda1e87c656e07
TheKenkei <nowikow.witaliy@gmail.com>
2024-05-30 01:40:48 +0300

    feat: add migration for `Services`

---
+++ migrations/00000000000004-services.js
@@ -0,0 +1,41 @@
+('use strict');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      length: {
+        type: Sequelize.NUMBER,
+      },
+      sum: {
+        type: Sequelize.NUMBER,
+      },
+      transport_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'transports',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  },
+};
