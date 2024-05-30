382b6ba8048d50ea366294b447af146dba8fd2bd
olegnowikow <nowolegik@gmail.com>
2024-05-30 18:33:06 +0300

    fix: use native sql

--- migrations/00000000000008-transports.js
+++ migrations/00000000000008-transports.js
@@ -2,9 +2,9 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('transports', 'avgConsumption', {
-      type: Sequelize.INTEGER,
-    });
+    await queryInterface.sequelize.query(
+      'ALTER TABLE transports ALTER COLUMN "avgConsumption" TYPE INTEGER USING ("avgConsumption"::integer);'
+    );
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.changeColumn('transports', 'avgConsumption', {
